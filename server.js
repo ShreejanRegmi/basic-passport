0bb5b83ff38023eed63fe067d7ca4cceeec71230
ShreejanRegmi <shreejan.regmi@gmail.com>
2020-10-17 20:23:33 +0545

    added first layout

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ const path = require('path')
 
 const app = express()
 
+app.set('view engine', 'ejs')
+
 const PORT = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-    res.send('hello')
+    res.render('index', {data: 'data from app'})
 })
 
 app.listen(PORT, console.log(`listening on port ${PORT}`))
