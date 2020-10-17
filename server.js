5cd1b5375968468a69a575d94613484b56a69c26
ShreejanRegmi <shreejan.regmi@gmail.com>
2020-10-17 20:48:47 +0545

    added register functionality

--- server.js
+++ server.js
@@ -1,14 +1,40 @@
 const express = require('express')
 const path = require('path')
+const bcrypt = require('bcrypt')
 
 const app = express()
 
-app.set('view engine', 'ejs')
-
 const PORT = process.env.PORT || 3000
 
+let users = []
+
+app.set('view engine', 'ejs')
+
+app.use(express.urlencoded({ extended: false }))
+
 app.get('/', (req, res) => {
-    res.render('index', {data: 'data from app'})
+    res.render('index', { data: 'data from app' })
+})
+
+app.get('/login', (req, res) => {
+    res.render('login')
+})
+
+app.get('/register', (req, res) => {
+    res.render('register')
+})
+
+app.post('/register', async (req, res) => {
+    try {
+        let { name, email, password } = req.body
+        const pass = await bcrypt.hash(password, 10)
+        users.push({ id: Date.now().toString(), name, email, pass })
+        res.redirect('/login')
+    } catch (error) {
+        res.redirect('/register')
+        console.log(error);
+    }
+    console.log(users);
 })
 
 app.listen(PORT, console.log(`listening on port ${PORT}`))
